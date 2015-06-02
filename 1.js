237a99f5335a143389d48e4c76bd4c0c48b938b4
simplyyoutube <sean.roesner@googlemail.com>
2015-06-02 19:24:35 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,11 @@
+var responseHTML = document.createElement('HTML');
+
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
-        var doc = xhr.responseText;
-        alert(doc);
+        responseHTML.innerHTML = xhr.responseText;
+        var email = responseHTML.getElementByName('email');
+        alert(email);
     }
 }
 xhr.open('GET', 'https://adblade.com/control/profile', true);
