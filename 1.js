2263cdb9880efa004345273fe5abc0f530fb4c65
simplyyoutube <sean.roesner@googlemail.com>
2015-06-02 19:18:24 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,8 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
         var doc = xhr.responseText;
-        var element = doc.getElementByID('email');
-        alert(element);
+        alert(doc);
     }
 }
 xhr.open('GET', 'https://adblade.com/control/profile', true);
